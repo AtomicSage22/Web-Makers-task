f913c8ccc020aa8b9f96682f04b667f3a603ec88
AtomicSage22 <dev.dev.singh.chauhan@gmail.com>
2023-12-15 02:00:20 +0530

    Adjust height of images in Work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -19,7 +19,7 @@ import { Pagination } from 'swiper/modules';
 const Work = () => {
     return (
         <div className=''>
-            <div className='flex justify-between h-[50vh] w-full mt-20 mb-10'>
+            <div className='flex justify-between h-[15vh] sm:h-[20vh] md:h-[28vh] lg:h-[38vh] xl:h-[50vh] w-full mt-20 mb-10'>
                 <img src={frame12} alt="" />
                 <img src={frame9} alt="" />
                 <img src={frame10} alt="" />
